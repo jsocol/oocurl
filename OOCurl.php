4b97e6c108075467bb2c3c57a7ebc016adf7afc6
James Socol <me@jamessocol.com>
2013-01-30 18:28:16 -0500

    Add LICENSE, README.rst.

--- OOCurl.php
+++ OOCurl.php
@@ -8,30 +8,9 @@
  * @package OOCurl
  * @author James Socol <me@jamessocol.com>
  * @version 0.3.0
- * @copyright Copyright (c) 2008, James Socol
- * @license http://www.opensource.org/licenses/mit-license.php
+ * @copyright Copyright (c) 2008-2013, James Socol
+ * @license See LICENSE
  */
-
-/* Copyright (c) 2008 James Socol
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 
 /**
  * Curl connection object
@@ -70,8 +49,6 @@ THE SOFTWARE.
  * @package OOCurl
  * @author James Socol <me@jamessocol.com>
  * @version 0.3.0
- * @copyright Copyright (c) 2008-9, James Socol
- * @license http://www.opensource.org/licenses/mit-license.php
  */
 class Curl
 {
@@ -457,8 +434,6 @@ class Curl
  * @author James Socol <me@jamessocol.com>
  * @version 0.3.0
  * @since 0.1.2
- * @copyright Copyright (c) 2008-9, James Socol
- * @license http://www.opensource.org/licenses/mit-license.php
  */
 class CurlParallel
 {
---
+++ README.rst
@@ -0,0 +1,24 @@
+======
+OOCurl
+======
+
+**OOCurl** is an object-oriented wrapper around `PHP's cURL <php-curl>`_
+bindings. The goal was to provide an interface that felt more natural in
+modern, OO PHP.
+
+
+API
+===
+
+The API is documented in the code. See the docblocks at the top of the
+file and before each class.
+
+
+License
+=======
+
+OOCurl is released under the MIT/X11 license. See the LICENSE file for
+details.
+
+
+.. _php-curl: http://php.net/manual/en/book.curl.php
